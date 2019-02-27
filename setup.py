df7888660c7de637abac0d682bdb9404a0e8f162
klemen_git <zaletelj.klemen@gmail.com>
2019-02-27 13:34:06 +0100

    prep for 0.18 release

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ For the showcase see: https://github.com/ladisk/pyEMA/blob/master/pyEMA%20Showca
 
 from setuptools import setup
 setup(name='pyEMA',
-      version='0.17',
+      version='0.18',
       author='Klemen Zaletelj, Tomaž Bregar, Domen Gorjup, Janko Slavič',
       author_email='janko.slavic@fs.uni-lj.si, ladisk@gmail.com',
       description='Experimental and operational modal analysis.',
       url='https://github.com/ladisk/pyEMA',
       py_modules=['pyEMA'],
       long_description=desc,
-      install_requires=['numpy', 'tqdm', 'scipy', 'matplotlib']
+      install_requires=['numpy>=1.15.4', 'tqdm', 'scipy>=1.1.0', 'matplotlib>=3.0.0']
       )
