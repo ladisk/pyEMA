95124e5252966ca5413ce8aa91a57945709e6736
klemen_git <zaletelj.klemen@gmail.com>
2019-03-05 12:55:59 +0100

    Test update

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,15 +11,15 @@ def test_1():
     freq, H1_main = np.load("./data/acc_data.npy")
     FRF = H1_main[:,1,:]
     freq = freq
-    acc = pyEMA.lscf(frf=FRF, freq=freq, lower=50, 
+    acc = pyEMA.lscf(frf=FRF, freq=freq, lower=10, 
                     upper=1000, pol_order_high=60)
     
     acc.get_poles()
     
-    n_freq = [175., 476.]
+    n_freq = [176., 476.]
     acc.select_closest_poles(n_freq)
-    np.testing.assert_almost_equal(acc.nat_freq[0], 176.0508429408248)
-    np.testing.assert_almost_equal(acc.nat_freq[1], 476.4662361973787)
+    np.testing.assert_almost_equal(acc.nat_freq[0], 177.0476572, decimal=5)
+    np.testing.assert_almost_equal(acc.nat_freq[1], 477.4667040, decimal=5)
     
     H, A = acc.lsfd(whose_poles='own', FRF_ind='all')
     assert A.shape[0]==6
