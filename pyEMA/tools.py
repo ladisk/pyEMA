0689cdddf8b126e31204a2bdc4b302609a4f6ba3
klemen_git <zaletelj.klemen@gmail.com>
2020-04-06 16:08:38 +0200

    complex to normal mode function

--- pyEMA/tools.py
+++ pyEMA/tools.py
@@ -43,3 +43,41 @@ def MAC(phi_X, phi_A):
                             (np.conj(phi_X[:, i]) @ phi_X[:, i] *\
                             np.conj(phi_A[:, j]) @ phi_A[:, j])
     return MAC
+
+
+def complex_to_normal_mode(mode):
+    """Transform a complex mode shape to normal mode shape.
+    
+    The real mode shape should have the maximum correlation with
+    the original complex mode shape. The vector that is most correlated
+    with the complex mode, is the real part of the complex mode when it is
+    rotated so that the norm of its real part is maximized. [1]
+    
+    Literature:
+        [1] Gladwell, H. Ahmadian GML, and F. Ismail. 
+            "Extracting Real Modes from Complex Measured Modes."
+    
+    :params mode: np.ndarray, a mode shape to be transformed. Can contain a single
+        mode shape or a modal matrix `(n_locations, n_modes)`.
+    :return: normal mode shape
+    """
+    if mode.ndim == 1:
+        mode = mode[None, :, None]
+    elif mode.ndim == 2:
+        mode = mode.T[:, :, None]
+    else:
+        raise Exception(f'`mode` must have 1 or 2 dimensions ({mode.ndim}).')
+
+    # Normalize modes so that norm == 1.0
+    mode = np.array([m/np.linalg.norm(m) for m in mode])
+    
+    mode_T = np.transpose(mode, [0, 2, 1])
+
+    U = np.real(mode) @ np.real(mode_T) + np.imag(mode) @ np.imag(mode_T)
+    
+    val, vec = np.linalg.eig(U)
+    i = np.argmax(np.real(val), axis=1)
+
+    normal_mode = np.real([v[:, _] for v, _ in zip(vec, i)]).T
+
+    return normal_mode
