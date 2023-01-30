93715aa7bd0f8cfc3fa8a43492391bbf2cd80889
klemengit <zaletelj.klemen@gmail.com>
2023-01-30 12:55:34 +0100

    remove 'moving to sdypy-ema' warning

--- sdypy/EMA/__init__.py
+++ sdypy/EMA/__init__.py
@@ -7,6 +7,4 @@ from . import stabilization
 from . import normal_modes
 from . import pole_picking
 
-# pyEMA moving to SDyPy warning
 import warnings
-warnings.warn('This is the last version of pyEMA and will not longer be maintained since it is moving to the SDyPy package. To use the latest code from SDyPy: `pip install sdypy` and `from sdypy import EMA`.')
