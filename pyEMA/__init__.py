872d40029146b37919e6d44248b871223a7cc500
klemengit <zaletelj.klemen@gmail.com>
2024-11-19 13:22:17 +0100

    Update version to 0.26.1 and migrate imports to SDyPy package

--- pyEMA/__init__.py
+++ pyEMA/__init__.py
@@ -1,12 +1,18 @@
-__version__ = "0.26"
+__version__ = "0.26.1"
 
-from .pyEMA import Model
-from .tools import *
+# from .pyEMA import Model
+# from .tools import *
+# from . import stabilization
+# from . import normal_modes
+# from . import pole_picking
 
-from . import stabilization
-from . import normal_modes
-from . import pole_picking
+# Importing code from SDyPy-EMA package where it is maintained
+from sdypy.EMA import Model
+from sdypy.EMA.tools import *
+from sdypy.EMA import stabilization
+from sdypy.EMA import normal_modes
+from sdypy.EMA import pole_picking
 
 # pyEMA moving to SDyPy warning
 import warnings
-warnings.warn('This is the last version of pyEMA and will not longer be maintained since it is moving to the SDyPy package. To use the latest code from SDyPy: `pip install sdypy` and `from sdypy import EMA`.')
+warnings.warn('The imported code is channeled from the SDyPy package. It is recommended to use the SDyPy package instead of pyEMA. To use the latest code from SDyPy: `pip install sdypy` and `from sdypy import EMA`.', DeprecationWarning)
